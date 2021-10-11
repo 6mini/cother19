1610a69c0ee996fc354b8e8885cd7b3fd7fec75d
6mini <real6mini@gmail.com>
2021-10-11 23:45:41 +0900

    Edit README.md, 404page

--- README.md
+++ README.md
@@ -4,9 +4,40 @@
 
 </div>
 
-# CO-THER 19
-[Data Pipeline 구축 Project] 기상으로 알아보는 코로나 확진자 수 예측 프로그램
+![Project3 001](https://user-images.githubusercontent.com/79494088/136796271-f4a3aff3-a3a3-4137-a4b0-857faa3628a7.jpeg)
+
+<div align=center>
+
+<img src="https://img.shields.io/badge/HTML5
+-E34F26?style=flat-square&logo=HTML5
+&logoColor=white"/></a>
+<img src="https://img.shields.io/badge/Jupyter-F37626?style=flat-square&logo=Jupyter&logoColor=white"/></a>
+<img src="https://img.shields.io/badge/JavaScript
+-F7DF1E?style=flat-square&logo=JavaScript
+&logoColor=white"/></a>
+<img src="https://img.shields.io/badge/Python
+-3776AB?style=flat-square&logo=Python
+&logoColor=white"/></a>
+<img src="https://img.shields.io/badge/CSS3
+-1572B6?style=flat-square&logo=CSS3
+&logoColor=white"/></a>
+<img src="https://img.shields.io/badge/PostgreSQL
+-4169E1?style=flat-square&logo=PostgreSQL
+&logoColor=white"/></a>
+<img src="https://img.shields.io/badge/Amazon AWS-232F3E?style=flat-square&logo=AmazonAWS&logoColor=white"/></a>
+<img src="https://img.shields.io/badge/Heroku
+-430098?style=flat-square&logo=Heroku
+&logoColor=white"/></a>
+<img src="https://img.shields.io/badge/Flask-000000?style=flat-square&logo=Flask&logoColor=white"/></a>
+
+# Welcome to CO-THER 19 🙋🏻‍♂️
+
+</div>
+
+- [Data Pipeline 구축 Project] 기상으로 알아보는 코로나 확진자 수 예측 프로그램<br>
 (feat. AWS PostgreSQL, LGBM, Flask, Tableau)
+- [서비스 바로가기](https://cother.herokuapp.com/)
+- [블로그 포스팅 바로가기](https://6mini.github.io/project/2021/10/06/Cother/)
 
 ## History
 
@@ -19,20 +50,28 @@
 - LGBM 이용 다중회귀 모델링
 
 ### 10. 8. 1617i
-- model pickle 이용 import 및 export
-- html template 적용
+- Model Pickle 이용 import 및 export
+- HTML template 적용
 - 모델 활용 예측값 API 구현
 
 ### 10.10. 0619i
-- html, css 수정
+- HTML, CSS 수정
 - Dashboard 개발 및 배포 후 웹사이트 적용
 - GUI 최종 수정
-- favicon 추가
+- favicon.ico 추가
 
 ### 10.10. 1542i
 - 모바일 친화 작업
 - 최종 배포 및 git 정리
 - 404 page 추가
+
+### 10.11. 2209i
+- AWS EC2 Crontab 이용 DB 저장 자동화
+
+## Indroduction
+
+
+
 
 ## Preview
 
---
+++ data/py/postgres-weather-cronjob.py
@@ -0,0 +1,127 @@
+from datetime import datetime, timedelta
+import requests
+import psycopg2
+import logging
+import sys
+import json
+
+key = 'sMRl46KNdRk8LSFsSh1zJ9rfus4PM8hNLt1oOqVD6yhuPCagOupZN4U12BNVJobHswlBfVSV%2BfC4x3BSzaBnzw%3D%3D'
+yesterday = (datetime.today() - timedelta(1)).strftime("%Y%m%d")
+startDate = yesterday
+endDate = yesterday
+location = '108' # 서울
+
+
+host = "covid.cjwptwa04yyi.ap-northeast-2.rds.amazonaws.com"
+port = 5432
+username = "sixmini"
+database = "postgres"
+password = "12345678"
+
+
+def main():
+    try:
+        conn = psycopg2.connect(
+            host=host,
+            database=database,
+            user=username,
+            password=password)
+        cursor = conn.cursor()
+    except:
+        logging.error("could not connect to rds")
+        sys.exit()
+
+    API_URL = 'http://apis.data.go.kr/1360000/AsosDalyInfoService/getWthrDataList?serviceKey={}&pageNo=1&numOfRows=999&dataType=JSON&dataCd=ASOS&dateCd=DAY&startDt={}&endDt={}&stnIds={}'.format(key, startDate, endDate, location)
+    
+    r = requests.get(API_URL)
+    raw = json.loads(r.text)
+
+    weather_raw = raw['response']['body']['items']['item']
+    for i in range(len(weather_raw)):
+        weather = {}
+        try:
+            weather.update(
+                {
+                    'date': weather_raw[i]['tm'],
+                    'avgTa': weather_raw[i]['avgTa'],
+                    'minTa': weather_raw[i]['minTa'],
+                    'minTaHrmt': weather_raw[i]['minTaHrmt'],
+                    'maxTa': weather_raw[i]['maxTa'],
+                    'maxTaHrmt': weather_raw[i]['maxTaHrmt'],
+                    'mi10MaxRn': weather_raw[i]['mi10MaxRn'],
+                    'mi10MaxRnHrmt': weather_raw[i]['mi10MaxRnHrmt'],
+                    'hr1MaxRn': weather_raw[i]['hr1MaxRn'],
+                    'hr1MaxRnHrmt': weather_raw[i]['hr1MaxRnHrmt'],
+                    'sumRnDur': weather_raw[i]['sumRnDur'],
+                    'sumRn': weather_raw[i]['sumRn'],
+                    'maxInsWs': weather_raw[i]['maxInsWs'],
+                    'maxInsWsWd': weather_raw[i]['maxInsWsWd'],
+                    'maxInsWsHrmt': weather_raw[i]['maxInsWsHrmt'],
+                    'maxWs': weather_raw[i]['maxWs'],
+                    'maxWsWd': weather_raw[i]['maxWsWd'],
+                    'maxWsHrmt': weather_raw[i]['maxWsHrmt'],
+                    'avgWs': weather_raw[i]['avgWs'],
+                    'hr24SumRws': weather_raw[i]['hr24SumRws'],
+                    'maxWd': weather_raw[i]['maxWd'],
+                    'avgTd': weather_raw[i]['avgTd'],
+                    'minRhm': weather_raw[i]['minRhm'],
+                    'minRhmHrmt': weather_raw[i]['minRhmHrmt'],
+                    'avgRhm': weather_raw[i]['avgRhm'],
+                    'avgPv': weather_raw[i]['avgPv'],
+                    'avgPa': weather_raw[i]['avgPa'],
+                    'maxPs': weather_raw[i]['maxPs'],
+                    'maxPsHrmt': weather_raw[i]['maxPsHrmt'],
+                    'minPs': weather_raw[i]['minPs'],
+                    'minPsHrmt': weather_raw[i]['minPsHrmt'],
+                    'avgPs': weather_raw[i]['avgPs'],
+                    'ssDur': weather_raw[i]['ssDur'],
+                    'sumSsHr': weather_raw[i]['sumSsHr'],
+                    'hr1MaxIcsrHrmt': weather_raw[i]['hr1MaxIcsrHrmt'],
+                    'hr1MaxIcsr': weather_raw[i]['hr1MaxIcsr'],
+                    'sumGsr': weather_raw[i]['sumGsr'],
+                    'ddMefs': weather_raw[i]['ddMefs'],
+                    'ddMefsHrmt': weather_raw[i]['ddMefsHrmt'],
+                    'ddMes': weather_raw[i]['ddMes'],
+                    'ddMesHrmt': weather_raw[i]['ddMesHrmt'],
+                    'sumDpthFhsc': weather_raw[i]['sumDpthFhsc'],
+                    'avgTca': weather_raw[i]['avgTca'],
+                    'avgLmac': weather_raw[i]['avgLmac'],
+                    'avgTs': weather_raw[i]['avgTs'],
+                    'minTg': weather_raw[i]['minTg'],
+                    'avgCm5Te': weather_raw[i]['avgCm5Te'],
+                    'avgCm10Te': weather_raw[i]['avgCm10Te'],
+                    'avgCm20Te': weather_raw[i]['avgCm20Te'],
+                    'avgCm30Te': weather_raw[i]['avgCm30Te'],
+                    'avgM05Te': weather_raw[i]['avgM05Te'],
+                    'avgM10Te': weather_raw[i]['avgM10Te'],
+                    'avgM15Te': weather_raw[i]['avgM15Te'],
+                    'avgM30Te': weather_raw[i]['avgM30Te'],
+                    'avgM50Te': weather_raw[i]['avgM50Te'],
+                    'sumLrgEv': weather_raw[i]['sumLrgEv'],
+                    'sumSmlEv': weather_raw[i]['sumSmlEv'],
+                    'n99Rn': weather_raw[i]['n99Rn'],
+                    'iscs': weather_raw[i]['iscs'],
+                    'sumFogDur': weather_raw[i]['sumFogDur']
+                }
+            )
+            for i in weather:
+                if weather[i] == '':
+                    weather[i] = '0'
+            insert_row(cursor, weather, 'weather')
+        except:
+            print('some error!')
+            continue
+        
+    conn.commit()
+    
+
+def insert_row(cursor, data, table):
+    placeholders = ', '.join(['%s'] * len(data))
+    columns = ', '.join(data.keys())
+    key_placeholders = ', '.join(['{0}=%s'.format(k) for k in data.keys()])
+    sql = "INSERT INTO %s ( %s ) VALUES ( %s ) ON CONFLICT ( %s ) DO UPDATE SET  %s" % (table, columns, placeholders, list(data.keys())[0] ,key_placeholders)
+    cursor.execute(sql, list(data.values())*2)
+
+
+if __name__=='__main__':
+    main()
